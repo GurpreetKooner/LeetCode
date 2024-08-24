aa9e58568130b46492ca0ccb9a5986b0653b8563
Gurpreet Singh <40815653+GurpreetKooner@users.noreply.github.com>
2024-08-24 17:38:38 -0500

    Time: 0 ms (100%), Space: 41.8 MB (58.25%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,8 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        quickSort(nums, 0, nums.length-1);
+        // quickSort(nums, 0, nums.length-1);
+        int i=0, j=0, k=nums.length-1;
+
+        while(j<=k){
+            if(nums[j]==0){
+                swap(nums, i, j);
+                i++;
+                j++;
+            }else if(nums[j]==2){
+                swap(nums, j, k);
+                k--;
+            }else{
+                j++;
+            }
+        }
         
+    }
+
+    public void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
     public static void quickSort(int[] nums, int lo, int hi) {
